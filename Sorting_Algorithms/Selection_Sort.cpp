af3a6b8a271ac9da8244e6db101c09081772fd53
Sivasubramani <t.sivasubramani.5@gmail.com>
2021-09-20 10:21:19 +0530

    Update Selection_Sort.cpp

--- Sorting_Algorithms/Selection_Sort.cpp
+++ Sorting_Algorithms/Selection_Sort.cpp
@@ -11,7 +11,7 @@ using namespace std;
 int main(){
     vector<int> A {2,7,4,5,3,1,-23,34,56,-132,-1322,131,-131,-124,342,-65,-54,-35,43};
     int n= A.size();
-    for(int i=0;i<n;i++){
+    for(int i=0;i<n-1;i++){
         int minValue =A[i];
         for(int j=i+1;j<n;j++){
 
